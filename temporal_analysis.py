1afa1cd7acd80f240cde72a7de74af48822c17b0
stephanie <stephaniewhoo@hotmail.com>
2020-06-27 20:16:31 -0400

    change to 4 graphs

--- temporal_analysis.py
+++ temporal_analysis.py
@@ -1,52 +1,67 @@
-#
-# Pyserini: Python interface to the Anserini IR toolkit built on Lucene
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
 import streamlit as st
 import argparse
 import pandas as pd
+import datetime
 
 st.title('Cord-19 data analysis')
 read_and_cache_csv = st.cache(pd.read_csv)
 
 @st.cache
-def load():
-    df_url = "https://raw.githubusercontent.com/stephaniewhoo/streamlit/master/data_counts.csv"
+def load_all():
+    df_url = "https://raw.githubusercontent.com/stephaniewhoo/streamlit/master/date_05_26_df_counts.csv"
     df = pd.read_csv(df_url, error_bad_lines=False)
     df['publish_date'] = pd.to_datetime(df['publish_date'])
     return df
 
+def load():
+    df_url = "https://raw.githubusercontent.com/stephaniewhoo/streamlit/master/date_05_26_df_counts.csv"
+    df_only_month = pd.read_csv(df_url, error_bad_lines=False)
+    df_only_month = df_only_month.loc[df_only_month['publish_date'].str.len() > 4]
+    df_only_month['publish_date'] = pd.to_datetime(df_only_month['publish_date'])
+    return df_only_month
+
 data_load_state = st.text('Loading data...')
-df = load()
-data_load_state.text("Done! (using st.cache)")
+df = load_all()
+df_only_month = load()
+data_load_state.text("Done!")
 
 st.subheader('Number of published documents in each year')
-df_yearly = df.groupby(pd.Grouper(key='publish_date', freq='Y'))['counts_0512', 'counts_0519'].agg('sum').reset_index('publish_date')
-df_yearly = df.set_index('publish_date')
+start_year = st.slider('What is the start year you want to see distribution?', 1870, 2020, 2000)
+end_year = st.slider('What is the end year you want to see distribution?', 1871, 2021, 2020)
+df_yearly = df.loc[(df['publish_date'].dt.year >= start_year) & (df['publish_date'].dt.year <= end_year)]
+df_yearly = df_yearly.groupby(pd.Grouper(key='publish_date', freq='Y'))['counts'].agg('sum').reset_index('publish_date')
+df_yearly = df_yearly.set_index('publish_date')
 st.bar_chart(df_yearly)
 
-year = st.slider('Which year you want to see distribution in detail?', 1955, 2021, 2019)
-df_monthly = df.loc[df['publish_date'].dt.year == year]
-df_monthly = df_monthly.groupby(pd.Grouper(key='publish_date', freq='M'))['counts_0512', 'counts_0519'].agg('sum').reset_index('publish_date')
+st.subheader('Number of published documents in monthly granularity')
+start_month = st.date_input('what start date you want to see for bars in monthly granularity?', datetime.date(2019, 7, 1), datetime.date(1955, 10, 31),datetime.date(2021, 11, 30))
+end_month = st.date_input('what end date you want to see for bars in monthly granularity?', datetime.date(2020, 7, 1), datetime.date(1957, 8, 1), datetime.date(2021, 12, 31))
+df_monthly = df_only_month.loc[df_only_month['publish_date'].dt.date >= start_month]
+df_monthly = df_monthly.loc[df_monthly['publish_date'].dt.date <= end_month]
+df_monthly = df_monthly.groupby(pd.Grouper(key='publish_date', freq='M'))['counts'].agg('sum').reset_index('publish_date')
 df_monthly = df_monthly.set_index('publish_date')
 st.bar_chart(df_monthly)
 
-month = st.slider('Which year you want to see distribution in detail?', 1, 12, 6)
-df_weekly = df.loc[((df['publish_date'].dt.year == year) & (df['publish_date'].dt.month == month))]
-df_monthly = df_weekly.groupby(pd.Grouper(key='publish_date', freq='W'))['counts_0512', 'counts_0519'].agg('sum').reset_index('publish_date')
+
+st.subheader('Number of published documents in weekly granularity')
+start_week = st.date_input('what start date you want to see for bars in weekly granularity?', datetime.date(2020, 1, 1), datetime.date(1955, 10, 31), datetime.date(2021, 11, 30))
+end_week = st.date_input('what end date you want to see for bars in weekly granularity?', datetime.date(2020, 7, 1), datetime.date(1957, 8, 1), datetime.date(2021, 12, 31))
+df_weekly = df_only_month.loc[df_only_month['publish_date'].dt.date >= start_week]
+df_weekly = df_weekly.loc[df_weekly['publish_date'].dt.date <= end_week]
+df_weekly = df_weekly.groupby(pd.Grouper(key='publish_date', freq='W'))['counts'].agg('sum').reset_index('publish_date')
 df_weekly = df_weekly.set_index('publish_date')
 st.bar_chart(df_weekly)
+
+st.subheader('Number of published documents in daily granularity')
+start_day = st.date_input('what start date you want to see for bars in daily granularity?', datetime.date(2020, 3, 1), datetime.date(1955, 10, 31), datetime.date(2021, 11, 30))
+end_day = st.date_input('what end date you want to see for bars in daily granularity?', datetime.date(2020, 3, 31), datetime.date(1957, 8, 1), datetime.date(2021, 12, 31))
+df_daily = df_only_month.loc[df_only_month['publish_date'].dt.date >= start_day]
+df_daily = df_daily.loc[df_daily['publish_date'].dt.date <= end_day]
+df_daily = df_daily.set_index('publish_date')
+st.bar_chart(df_daily)
+
+
 
 
 
