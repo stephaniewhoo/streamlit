0ff4f8b477548b196251e40c44fb6f0797d41de0
stephanie <stephaniewhoo@hotmail.com>
2020-06-29 16:03:19 -0400

    change slider and plot

--- temporal_analysis.py
+++ temporal_analysis.py
@@ -1,6 +1,5 @@
 
 import streamlit as st
-import argparse
 import pandas as pd
 import datetime
 
@@ -27,16 +26,17 @@ df_only_month = load()
 data_load_state.text("Done!")
 
 st.subheader('Number of published documents in each year')
-start_year = st.slider('What is the start year you want to see distribution?', 1870, 2020, 2000)
-end_year = st.slider('What is the end year you want to see distribution?', 1871, 2021, 2020)
+start_year, end_year = st.slider("Select year Range:", 1870, 2021, (2003, 2020), 1)
 df_yearly = df.loc[(df['publish_date'].dt.year >= start_year) & (df['publish_date'].dt.year <= end_year)]
 df_yearly = df_yearly.groupby(pd.Grouper(key='publish_date', freq='Y'))['counts'].agg('sum').reset_index('publish_date')
+df_yearly['publish_date'] = df_yearly['publish_date'].dt.year
+df_yearly.plot.bar(x='publish_date', y='counts', width=0.9, figsize=(12, 8))
+st.pyplot()
 df_yearly = df_yearly.set_index('publish_date')
 st.bar_chart(df_yearly)
 
 st.subheader('Number of published documents in monthly granularity')
-start_month = st.date_input('what start date you want to see for bars in monthly granularity?', datetime.date(2019, 7, 1), datetime.date(1955, 10, 31),datetime.date(2021, 11, 30))
-end_month = st.date_input('what end date you want to see for bars in monthly granularity?', datetime.date(2020, 7, 1), datetime.date(1957, 8, 1), datetime.date(2021, 12, 31))
+start_month, end_month = st.date_input('Select date range to see monthly granularity plot', (datetime.date(2019, 7, 1), datetime.date(2020, 5, 1)), datetime.date(1955, 10, 31), datetime.date(2021, 12, 31))
 df_monthly = df_only_month.loc[df_only_month['publish_date'].dt.date >= start_month]
 df_monthly = df_monthly.loc[df_monthly['publish_date'].dt.date <= end_month]
 df_monthly = df_monthly.groupby(pd.Grouper(key='publish_date', freq='M'))['counts'].agg('sum').reset_index('publish_date')
@@ -45,8 +45,7 @@ st.bar_chart(df_monthly)
 
 
 st.subheader('Number of published documents in weekly granularity')
-start_week = st.date_input('what start date you want to see for bars in weekly granularity?', datetime.date(2020, 1, 1), datetime.date(1955, 10, 31), datetime.date(2021, 11, 30))
-end_week = st.date_input('what end date you want to see for bars in weekly granularity?', datetime.date(2020, 7, 1), datetime.date(1957, 8, 1), datetime.date(2021, 12, 31))
+start_week, end_week = st.date_input('Select date range to see weekly granularity plot', (datetime.date(2020, 1, 1), datetime.date(2020, 4, 1)), datetime.date(1955, 10, 31), datetime.date(2021, 12, 31))
 df_weekly = df_only_month.loc[df_only_month['publish_date'].dt.date >= start_week]
 df_weekly = df_weekly.loc[df_weekly['publish_date'].dt.date <= end_week]
 df_weekly = df_weekly.groupby(pd.Grouper(key='publish_date', freq='W'))['counts'].agg('sum').reset_index('publish_date')
@@ -54,8 +53,7 @@ df_weekly = df_weekly.set_index('publish_date')
 st.bar_chart(df_weekly)
 
 st.subheader('Number of published documents in daily granularity')
-start_day = st.date_input('what start date you want to see for bars in daily granularity?', datetime.date(2020, 3, 1), datetime.date(1955, 10, 31), datetime.date(2021, 11, 30))
-end_day = st.date_input('what end date you want to see for bars in daily granularity?', datetime.date(2020, 3, 31), datetime.date(1957, 8, 1), datetime.date(2021, 12, 31))
+start_day, end_day= st.date_input('Select date range to see daily granularity plot', (datetime.date(2020, 2, 1), datetime.date(2020, 3, 1)), datetime.date(1955, 10, 31), datetime.date(2021, 12, 31))
 df_daily = df_only_month.loc[df_only_month['publish_date'].dt.date >= start_day]
 df_daily = df_daily.loc[df_daily['publish_date'].dt.date <= end_day]
 df_daily = df_daily.set_index('publish_date')
